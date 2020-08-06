4cde535dc1ab6cd14016416aa5407b2535fbd219
Mickocar <milovan.maric@gmail.com>
2020-08-05 20:56:30 -0400

    add GitFun.java

---
+++ src/GitFun.java
@@ -0,0 +1,7 @@
+public class GitFun {
+
+    public static void main(String[] args) {
+        System.out.println("I am fun");
+    }
+
+}
--- src/ReverseAString.java
+++ src/ReverseAString.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class ReverseAString {
 
     public static void main(String[] args) {
-
-
-        String str="Milovan";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a String: ");//cat
+        String str = scan.nextLine();
+        //char first_ch = str.charAt(0);
+        //String last_ch = str.substring(str.length()- 1);
         String reverse = "";
-        for (int i=str.length()-1;i<=0;i++){
-            reverse+=i;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            reverse += ch;
         }
         System.out.println(reverse);
     }
